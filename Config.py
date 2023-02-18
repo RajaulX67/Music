506de34bcbc12d32a55985ea28d7ae9c0121ddb8
Rajaul Haque <122172248+RajaulX67@users.noreply.github.com>
2023-02-18 21:50:44 +0600

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6435225"))
-    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
-    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
-    HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    SUPPORT = os.environ.get("SUPPORT", "SankiWorldMF") # Your Support
-    CHANNEL = os.environ.get("CHANNEL", "NixaWorld") # Your Channel
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5688281798")) # telegram I'd not Username
-    AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
-    AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
+    API_ID = int(6435225) #dont change
+    API_HASH = "4e984ea35f854762dcde906dce426c2d" #dont change 
+    BOT_TOKEN = "6114413915:AAESpq6GawEJc7rmQwk__-ggu3e4b6RL9FE" #fill your telegram bot token
+    STRING_SESSION = "1BVtsOL8Bu4YumL0WzelJ5o-DWC9pJtz2pw1VIkFEHudIjXoIzQjSjkkc6m2ZcBl12j6H6ydvwVS0DV2tNmzvSSNApxgXJKPRb-_E3zYa8VmNMe8YhKtx1LErXQXnJFcExIf0RefbIMZmmr9Sptt6IuSB5JGCs3NRFh6j5FdfkV8tkMWwpouPhdS5bN-yD1d6V8LcFtPpbT6Qn3iWK4cXtwMGtJuLR0Y-hGrk58q95qFhW7AjDhxf6_7spnKIMw1cGbr9wSH2azgX6_DRQu6GgVDSej-C-3sJb6jM7xDlbIsUCuxW4FkNBkGXvsZw2yueHm6gTlrwEICf4hlAF65hL0WIE23kJhg=" #an Telethon Session 
+    MANAGEMENT_MODE = None #Dont change
+    HEROKU_MODE = None #dont change
+    BOT_USERNAME = "LinuxMusicBot" #Your Bot username 
+    SUPPORT = "SankiWorldMF" #optional
+    CHANNEL = "NixaWorld" #optional
+    START_IMG = "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png"
+    CMD_IMG = "https://telegra.ph/file/66518ed54301654f0b126.png"
+    ASSISTANT_ID = int(5688281798) #your string added account user id..
+    AUTO_LEAVE_TIME = int(54000) # in seconds
+    AUTO_LEAVE = True #Optional
